3b52393d7f7b1ac0d3d8c1c755d226aa50939e0a
calli3k <calli3k@live.unc.edu>
2020-11-20 20:25:57 -0500

    Updated the profile css

--- collegechecklist/src/Components/ProfilePage.jsx
+++ collegechecklist/src/Components/ProfilePage.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { UserContext } from "../Providers/UserProvider";
 import { navigate } from "@reach/router";
 import {auth} from "../firebase";
+
 const ProfilePage = () => {
   const user = useContext(UserContext);
   const {photoURL, displayName, email} = user;
@@ -9,23 +10,49 @@ const ProfilePage = () => {
   
   return (
     <div className = "mx-auto w-11/12 md:w-2/4 py-8 px-4 md:px-8">
-      <div className="flex border flex-col items-center md:flex-row md:items-start border-blue-400 px-3 py-4">
-        <div
-          style={{
-            background: `url(${photoURL || 'https://res.cloudinary.com/dqcsk8rsc/image/upload/v1577268053/avatar-1-bitmoji_upgwhc.png'})  no-repeat center center`,
-            backgroundSize: "cover",
-            height: "200px",
-            width: "200px"
-          }}
-          className="border border-blue-300"
-        ></div>
-        <div className = "md:pl-4">
-        <h2 className = "text-2xl font-semibold">{displayName}</h2>
-        <h3 className = "italic">{email}</h3>
+      <section className="container">
+        <header>
+          <a className="icon" href="https://bestcollegeaid.com"><img src="BCA_logo.png" alt= "best-college-aid-logo"></img></a>
+        </header>
+
+    <main>
+      <div className = "login">
+        <div className = "photobox">
+          <div
+            style={{
+              background: `url(${photoURL || 'https://res.cloudinary.com/dqcsk8rsc/image/upload/v1577268053/avatar-1-bitmoji_upgwhc.png'})  no-repeat center center`,
+              backgroundSize: "cover",
+              height: "200px",
+              width: "200px"
+            }}
+
+            className="border border-blue-300"
+
+          ></div>
+          <div style = {{textAlign: "center"}}> 
+          <field>
+            <button>Change Photo</button>
+          </field>
+          </div>
         </div>
+        <br></br>
+        <div className = "md:pl-4">
+        <h1 className = "text-2xl font-semibold">{displayName}</h1>
+        <h1 className = "italic">{email}</h1>
+        </div> 
+        <br></br>
+        <input className = "submit" type="submit" value = "Return to Checklist"/>
+        <button className = "submit" onClick = {() => {auth.signOut()}}>Sign out</button>
+
       </div>
-      <button className = "w-full py-3 bg-red-600 mt-4 text-white" onClick = {() => {auth.signOut()}}>Sign out</button>
+      </main>
+
+      <footer>
+        <h2>Best College Aid</h2>
+      </footer>
+      </section>
     </div>
+    
   ) 
 };
 export default ProfilePage;
