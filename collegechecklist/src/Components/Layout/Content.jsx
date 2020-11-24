d386ec5b8abe4f26745e69bc05725ac73b4ebdc2
calli3k <calli3k@live.unc.edu>
2020-11-23 20:53:57 -0500

    Merged sidebar+main, changed checkbox styling

--- collegechecklist/src/Components/Layout/Content.jsx
+++ collegechecklist/src/Components/Layout/Content.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { Sidebar } from './Sidebar';
 import { MainContent } from "./MainContent";
 
 export const Content = () => (
   <section className="content-wrapper">
-    <Sidebar />
     <MainContent />
   </section>
 );
--- collegechecklist/src/Components/Layout/MainContent.jsx
+++ collegechecklist/src/Components/Layout/MainContent.jsx
@@ -4,16 +4,23 @@ import { SelectedCollegeProviderValue } from "../../Context";
 import { AddTask } from "../AddTask";
 import { CollegesProviderValue } from "../../Context";
 import { CheckBox } from "../Checkbox";
+import { Colleges } from '../Colleges';
+import { AddCollege } from '../AddCollege';
+
+
 
 export const MainContent = () => {
-
+  const { selectedCollege, setSelectedCollege,setSelectedCollegeName, selectedCollegeName } = SelectedCollegeProviderValue();
+  const [hideColleges, setHideColleges] = useState(false);
   const { colleges } = CollegesProviderValue();
-  const {
-    selectedCollege,
-    selectedCollegeName
-    
-  } = SelectedCollegeProviderValue();
+  const [show, setShow] = useState(false);
+
   const { tasks, archivedTasks } = useTasks(selectedCollege);
+
+  const quickShow = data => {
+    setShow(data);
+  };
+  
   const [showArchived, setShowArchived] = useState(false);
 
   const importedTasks = () => {
@@ -21,7 +28,80 @@ export const MainContent = () => {
 
   
     return (
+  
+
+
       <div className="allTasks">
+        <div className="list-holder">
+        <div className="top-section">
+
+        </div>
+
+        <div className="middle-section">
+
+          <div className="fullchecklist">
+            <div className="todolisttitle"><strong>To Do List</strong></div>
+            <div>
+              {colleges.map(college => (
+                <ul key={colleges.collegeID}>{college.name}
+                  {tasks.map(function (task) {
+                    if (task.collegeID === college.collegeID) {
+                      return (<li className="bulletlist" key={task.taskID}>
+                        <CheckBox id={task.taskID} />
+                        {task.task}
+                      </li>)
+                    }
+                  })}
+                </ul>))}
+
+            </div>
+          </div>
+          <div className="projects">
+            <div className="project-head">
+              <div
+                className="projects-title"
+                onClick={() => {
+                  setHideColleges(!hideColleges);
+                }}
+              >
+                <span className="title">Colleges</span>
+                <div
+                  className="inbox"
+                  onClick={() => {
+                    setSelectedCollege("INBOX");
+                    setSelectedCollegeName("Inbox");
+                  }}
+                >
+                  <div className="first-item">
+                    <span className="inboxtitle">Inbox</span>
+                  </div>
+                </div>
+              </div>
+              <span
+                className="quick-show"
+                onClick={() => {
+                  setShow(!show);
+                }}
+              >
+                <i className="fas fa-plus"></i>
+              </span>
+            </div>
+
+            <div
+              className={`hide-list-wrapper  ${hideColleges ? "hide" : ""} ${show ? "show" : ""
+                }`}
+            >
+              <div className="outer-projects">
+                <Colleges />
+              </div>
+              <div className="add-project">
+                <AddCollege value={quickShow} showState={show} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <br></br>
         <div className="tasks">
           <AddTask value={selectedCollege} />
           <hr />
